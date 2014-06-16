5617b94d08fc1c95373d8353e1ad24ba9fa75015
Anthony Zotti <theillbehaviored@gmail.com>
2014-06-16 17:05:12 -0400

    First real rails app

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get "/" => "galleries#index"
+  get "/galleries/new" => "galleries#new"
+  get "/galleries/:id/edit" => "galleries#edit"
+  get({"/galleries/:id" => "galleries#show"})
+  post "/galleries" => "galleries#create"
+  patch "/galleries/:id" => "galleries#update"
+  delete "/galleries/:id" => "galleries#destroy"
+end
+
