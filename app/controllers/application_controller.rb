822113c8e74f34f5b6ccc4ae5ffb1accc5760fc6
amzotti <azotti1337@gmail.com>
2014-12-31 07:25:26 -0500

    Extract creating search object into ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :make_search
+  before_action :require_login
   include Monban::ControllerHelpers
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :require_login
+  def make_search
+    @search = Search.new
+  end
 end
